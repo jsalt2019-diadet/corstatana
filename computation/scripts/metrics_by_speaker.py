7946bc63a027d98eb6f3a631027819bd8b1b04e0
karadayi <julien.karadayi@gmail.com>
2019-07-09 16:36:01 +0200

    accumulate results - reference side (untested)

--- computation/scripts/metrics_by_speaker.py
+++ computation/scripts/metrics_by_speaker.py
@@ -6,6 +6,19 @@
     of a Speaker Diarization System, and output several metrics at different
     granularity (file level or speaker level.
     This script wraps several pyannote-metrics and pyannote-core functions.
+    The output is an ascii file with the following table filled out:
+             ________________________________________________________________
+            |        |              |               Reference                |
+            |________|______________|________________________________________|
+            |        |              | Speaker   | other speaker | no speaker |
+            |________|______________|___________|_______________|____________|
+            |        |   Speaker    | Correct   | F.A. speaker  | F.A. speech|
+            |        |______________|___________|_______________|____________|
+            | System | Other Speaker| M. speaker|    other      |   other    |
+            |        |______________|___________|_______________|____________|
+            |        |  No Speaker  | M. speech |    other      |   other    |
+            |________|______________|___________|_______________|____________|
+
 """
 
 import os
@@ -14,14 +27,40 @@ import argparse
 import pyannote
 import pyannote-metrics
 
+from collections import defaultdict
 from pyannote.database.util import load_rttm
 from pyannote.core import Segment, Timeline, Annotation
 from pyannote.metrics.detection import DetectionErrorRate
 from pyannote.metrics.diarization import DiarizationErrorRate
 
+def get_mapping(system, reference):
+    """ get speaker mapping between system and reference"""
 
+    metric = DiarizationErrorRate()
+    mapping = metric.optimal_mapping(reference, system)
 
+    return mapping
 
+def accumulate_reference(r_labels, s_labels, mapping):
+    """ Using the mapping, fill the first column of the results table"""
+    correct = defaultdict(int)
+    miss_spk = defaultdict(int)
+    miss_speech = defaultdict(int)
+
+    for r_spk in r_labels: 
+        for s_spk in s_labels:
+            r_label = r_labels[r_spk]
+            s_label = s_labels[s_spk]
+            # iterate on segments in common in reference and system
+            for r, h in r_label.co_iter(s_label):
+                if r_spk == mapping[r_spk]:
+                    correct[r_spk] += (r & h).duration
+                else:
+                    miss_spk[r_spk] += (r & h).duration
+            # iterate on segments in common in reference and system_silences
+            for r, h_ in reference.co_iter(s_label.gaps()):
+                miss_speech[r_spk] += (r & h_).duration
+    return correct, miss_spk, miss_speech
 def write_evaluation(results):
     ''' Write the results in a table reporting the time spent in 
         each of the following cell:
@@ -66,7 +105,21 @@ def main():
     protocol = get_protocol(args.protocol)
 
     items = list(getattr(protocol, subset)())
-    reference = {item['uri']: item['annotated'] for item in items}
+    reference = {item['uri']: item['annotation'] for item in items}
+
+    for uri in reference: 
+        # preffix r: reference
+        # prefix s: system
+        r_annot = reference[uri]
+        s_annot = system[uri]
+
+        r_labels = {lab: r_annot.label_timeline[lab] for lab in r_annot.labels()}
+        s_labels = {lab: s_annot.label_timeline[lab] for lab in s_annot.labels()}
+
+        mapping = get_mapping(system, reference)
+        
+        # accumulate results, reference side
+        correct, miss_spk, miss_speech = accumulate_reference(r_labels, s_labels, mapping)
 
     # evaluate each wav referenced in system:
     # IF not vad:
