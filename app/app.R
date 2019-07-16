071089e58b47d98c41e4cb67a671f4d5fbbe45de
alecristia <alecristia@gmail.com>
2019-07-16 15:34:55 -0400

    leaderboard det in pregress

--- app/app.R
+++ app/app.R
@@ -3,6 +3,8 @@
 # alecristia@gmail.Com
 
 ### TODO
+# add talker detection
+# incorporate name+description using accompanying txt file
 
 # i did the following to get the file lists
  # grep "Mix" /export/fs01/jsalt19/output_rttms/pipeline/v1/lda120_plda_voxceleb/jsalt19_spkdiar_ami_eval_Mix-Headset/plda_scores_tbest/result.pyannote-der | grep -v "arn" | cut -f 1 -d " " > ami_test.txt
@@ -10,45 +12,6 @@
  # grep "_" /export/fs01/jsalt19/output_rttms/pipeline/v1/lda120_plda_voxceleb/jsalt19_spkdet_babytrain_eval_test/plda_scores_tbest/result.pyannote-der  | grep -v "arn" | cut -f 1 -d " " > babytrain_test.txt
  # grep "_U01" /export/fs01/jsalt19/output_rttms/pipeline/v1/lda120_plda_voxceleb/jsalt19_spkdiar_chime5_eval_U01/plda_scores_tbest/result.pyannote-der  | grep -v "arn" | cut -f 1 -d " " > chime5_test.txt
 
-# Also, files should be clean, with no warnings: so have the demon do:
-  # find ./ -name result.pyannote* > res.txt
-  # cat res.txt | while read -r line ; do
-  # clean=`echo $line | sed "s/result/clean/"`
-  # wn=`grep "hypoth" $line | wc -l | awk '{print $1}'`
-  # #echo $wn
-  # if [ $wn -eq 0 ] ; then
-  # #echo $clean
-  #   grep -v "arn" $line > $clean
-  # fi
-  # done
-
-# And also in R
-#read file list
-# fl=NULL
-# for(f in dir(pattern="test.txt"))    fl <- rbind(fl,cbind(f,read.table(f)))
-# mydir="../system_eval/output_rttms/"
-# allres=NULL
-# for(res in dir(mydir, recursive=T,pattern="clean.pyannote")){
-#   file_eval <- read_table(paste0(mydir,res), comment = "--")
-# 
-#   #the first col must be renamed
-#   colnames(file_eval)[1]<-"file"
-# 
-#   #standardize out
-#   if(length(grep("diarization",colnames(file_eval)))==1) {
-#     colnames(file_eval)[grep("diarization",colnames(file_eval))]<-"main"
-#     task="diar"} else {
-#       colnames(file_eval)[grep("detection",colnames(file_eval))]<-"main"
-#       task="det"}
-#   #clean and write
-#   file_eval=subset(file_eval,main!="Total")
-#   allres=rbind(allres,cbind(as.data.frame(file_eval)[,c("file","main")],res,task))
-# } #allres has 24108 obs
-# allres=merge(allres,fl,by.x="file",by.y="V1",all=T)
-# allres$corpus=gsub("_.*","",allres$f)
-# for(thisc in c("babytrain","ami","chime5","sri")) allres$main=gsub(thisc,"",allres$main)
-# allres[!is.na(allres$corpus),]->allres #attn removing files not assigned to a corpus we go from 23k to 9k results
-# write.table(allres,"allres.txt",row.names = F)
 
 
 library(shiny)
@@ -95,8 +58,11 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-  x <- getURL("https://raw.githubusercontent.com/jsalt-coml/corstatana/master/demon/allres.txt")
-  allres <- read.csv(text = x)
+  myurl <- getURL("https://raw.githubusercontent.com/jsalt-coml/corstatana/master/demon/allres.txt")
+  allres <- read.csv(text = myurl)
+  allres$res=as.character(allres$res)
+  for(thisc in levels(allres$corpus)) allres$res=gsub(thisc,"",allres$res)
+  allres$res=gsub("_clean.*","",allres$res)
 
    output$rank <- renderPlot({
      # generate bins based on input$bins from ui.R
--- demon/ldb-update.sh
+++ demon/ldb-update.sh
@@ -3,7 +3,7 @@ find /export/fs01/jsalt19/output_rttms/ -name result.pyannote* > newfilelist.txt
 diff filelist.txt newfilelist.txt | grep ">" | sed "s/> //" > dif.txt
 nlines=`wc -l dif.txt | awk '{print $1}'`
 
-#if there are new lines
+#if there are new lines for VAD or diarization
 if [ $nlines -gt 0 ] ; then
 
   #generate clean versions for the new files
@@ -23,11 +23,46 @@ if [ $nlines -gt 0 ] ; then
    #remove used res logs
    rm res/*
 
-   #push it online
-   git pull
-   git commit -a -m "updated file"
-   git push
-
    #replace the file list file
    mv newfilelist.txt filelist.txt
+
+   #push it online
+   git pull
+   git commit -a -m "updated Diar file"
+   git push
+
+fi
+
+#REPEAT FOR TALKER DETECTION
+myfolder="/export/fs01/jsalt19/output_rttms/"
+#myfolder="/Users/acristia/Documents/gitrepos/corstatana/system_eval/2a.1.voxceleb_div2/"
+toreplace=`echo $myfolder | sed "s/\//_/g"`
+find $myfolder -name *results > newDetfilelist.txt
+
+diff Detfilelist.txt newDetfilelist.txt | grep ">" | sed "s/> //" > dif.txt
+nlines=`wc -l dif.txt | awk '{print $1}'`
+
+#if there are new lines for detection
+if [ $nlines -gt 0 ] ; then
+
+  #generate clean versions for the new files
+  cat dif.txt | while read -r line ; do 
+  clean=`echo $line | sed "s/results/clean/" | sed "s/\//_/g" | sed "s/$toreplace//"`
+  cp $line resDet/$clean
+  done
+
+   #generate new lines to append to the allresDet.txt file
+   grep -H "EER" resDet/* | sed "s/EER//" | tr -s ":" | tr ":" "\t" | tr -d " " | sed "s/DCF.*//" >> allresDet.txt
+
+   #remove used res logs
+   rm resDet/*
+
+   #replace the file list file
+   mv newDetfilelist.txt fileDetlist.txt
+
+   #push it online
+   git pull
+   git commit -a -m "updated Det file"
+   git push
+
 fi
